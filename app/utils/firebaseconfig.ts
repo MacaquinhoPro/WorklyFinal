f0902ae4ea216871fdf2c22844c7272203f8614b
Juan Pablo Restrepo <157374982+jp-rco@users.noreply.github.com>
2025-06-07 14:25:16 -0500

    .env

--- app/utils/firebaseconfig.ts
+++ app/utils/firebaseconfig.ts
@@ -3,14 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+
 const firebaseConfig = {
-  apiKey:            'AIzaSyCyJ09wZm8Dd_PAnLGhculO-5utkCVZGEA',
-  authDomain:        'workly-9872e.firebaseapp.com',
-  projectId:         'workly-9872e',
-  storageBucket:     'workly-9872e.firebasestorage.app',      
-  messagingSenderId: '452351847078',
-  appId:             '1:452351847078:web:6814f148110c2f792fe267',
-  measurementId:     'G-YY7KZDWX29',
+  apiKey:            process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain:        process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,      
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId:     process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
